dc6e2cc42c2acd6f4bc449d97c8804ec17428dbd
Crotshot <46968070+Crotshot@users.noreply.github.com>
2021-12-14 19:03:24 +0000

    .

--- RandomGeneratorApp/app/src/main/java/wit/assignments/randomgeneratorapp/activities/MainActivity.kt
+++ RandomGeneratorApp/app/src/main/java/wit/assignments/randomgeneratorapp/activities/MainActivity.kt
@@ -58,7 +58,6 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
     }
 
     private fun setCurrentFragment(fragment:Fragment/*, num : Int*/) =  supportFragmentManager.beginTransaction().apply {
